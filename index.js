0d1732152b6500a5dc78fc830bc079d6acbdd053
lucky0011198 <68720142+lucky0011198@users.noreply.github.com>
2021-07-25 14:38:29 +0530

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
-const express = require("express");
-const app = express();
-const port = 3000;
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+require("dotenv").config();
+const sgmail = require("@sendgrid/mail");
+sgmail.setApiKey(process.env.API_KEY);
+const message = {
+  to: "varkallaxmikant62@gmail.com",
+  from: "attendanceandmuchmore@gmail.com",
+  subject: "hellow from lucky",
+  text: "you done it man",
+  html: "<h1>you done it man</h1>",
+};
+sgmail
+  .send(message)
+  .then((response) => console.log("email sent", response))
+  .catch((error) => console.log(error.message));
